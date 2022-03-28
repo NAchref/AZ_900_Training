bb7021a372e938562f03f8df0d8638b7196f910e
NAchref <64566472+NAchref@users.noreply.github.com>
2022-03-29 00:03:57 +0100

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -1,11 +1,13 @@
-#install IIS(Internet Information Services)
+#Creating Resource Group(to hold all the things thats we need)
+az group create --name --location
+
+#Install IIS(Internet Information Services)
 dism  \online  \enable-feature  \featurename:IIS-webServerRole
 
-
-#open port Command : 
-  az Vm open-port
+#Open port Command : 
+az Vm open-port
  
-#open port 80 HTTP : 
+#Open port 80 HTTP : 
 az Vm open-port
 --name myVm
 --resource-group Learn-47dn3r45nke
